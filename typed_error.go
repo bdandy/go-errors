32d2d96bcc41e71ba7892a0b6102d62945350d1d
Bohdan Dymchenko <dmbohdan@gmail.com>
2021-11-03 00:44:39 +0200

    Merge errors.go into typed_error.go

--- typed_error.go
+++ typed_error.go
@@ -1,6 +1,7 @@
 package typed_errors
 
 import (
+	"errors"
 	"fmt"
 )
 
@@ -9,6 +10,42 @@ type TypedError interface {
 	Is(err error) bool
 	WithArgs(args ...interface{}) formatted
 	Wrap(err error) wrapped
+}
+
+// String implements TypedError
+type String string
+
+// Is implements errors.Is
+func (e String) Is(err error) bool {
+	var ee String
+	if !errors.As(err, &ee) {
+		return false
+	}
+
+	return e == ee
+}
+
+// wrapped implements error interface
+func (e String) Error() string {
+	return string(e)
+}
+
+// Wrap adds cause to the String error and return wrapped
+func (e String) Wrap(err error) wrapped {
+	return wrapped{
+		TypedError: e,
+		cause:      err,
+	}
+}
+
+// WithArgs returns new error which would be formatted
+// Note: args are not impact on errors.Is; multiple call WithArgs sets most recent ones
+// so if two errors has different arguments they still would be equal
+func (e String) WithArgs(args ...interface{}) formatted {
+	return formatted{
+		TypedError: e,
+		args:       args,
+	}
 }
 
 // formatted represents error with additional params for Sprintf format
