d63b5d104d543829a5c0cb5e6a1522d5428e2f7b
Bohdan Dymchenko <dmbohdan@gmail.com>
2021-11-02 23:39:36 +0200

    Add string.go

---
+++ string.go
@@ -0,0 +1,39 @@
+package typed_errors
+
+import "errors"
+
+// String is simple error type
+type String string
+
+// Is implements errors.Is
+func (e String) Is(err error) bool {
+	var ee String
+	if !errors.As(err, &ee) {
+		return false
+	}
+
+	return e == ee
+}
+
+// cause implements error interface
+func (e String) Error() string {
+	return string(e)
+}
+
+// Wrap adds cause to the String error and return cause
+func (e String) Wrap(err error) cause {
+	return cause{
+		err:   e,
+		cause: err,
+	}
+}
+
+// WithArgs returns new error which would be formatted
+// Note: args are not impact on errors.Is,
+// so if two errors has different arguments they still would be equal
+func (e String) WithArgs(args ...interface{}) formatted {
+	return formatted{
+		err:  e,
+		args: args,
+	}
+}
